21eff43d474e3812aee0a3d8179c340c913c9677
shingo.kawamoto <shingo.kawamoto@shingokawamotonoMacBook-Pro.local>
2025-12-14 23:36:46 +0900

    再度コンテナ起動時のエラー追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,8 +12,10 @@ done
 
 echo "Database is ready!"
 
-# データベースの作成とマイグレーション（既存の場合はスキップ）
-bundle exec rails db:prepare
+# データベースの作成とマイグレーション
+# bundle exec rails db:prepare
+bundle exec rails db:create
+bundle exec rails db:migrate
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
