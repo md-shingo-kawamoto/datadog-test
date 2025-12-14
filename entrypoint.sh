b247fbf582566c576c4bb57c8aad879fb7682462
shingo.kawamoto <shingo.kawamoto@shingokawamotonoMacBook-Pro.local>
2025-12-14 21:39:47 +0900

    entrypoint修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,9 +12,8 @@ done
 
 echo "Database is ready!"
 
-# データベースの作成とマイグレーション
-bundle exec rails db:create
-bundle exec rails db:migrate
+# データベースの作成とマイグレーション（既存の場合はスキップ）
+bundle exec rails db:prepare
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
